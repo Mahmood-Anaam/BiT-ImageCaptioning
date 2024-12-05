c53805d06fa37c375afe7feb22d056d327066fce
Mahmood-Anaam <eng.mahmood.anaam@gmail.com>
2024-12-06 01:06:38 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ def install_subpackage(subpackage_path):
     """
     Install a sub-package from its setup.py file.
     """
-    subprocess.check_call(["pip", "install", "-e", subpackage_path])
+    # subprocess.check_call(["pip", "install", "-e", subpackage_path])
+    os.system(f"pip install -e {subpackage_path}")
 
 # Define the path to the scene_graph_benchmark package
 scene_graph_path = os.path.join(
